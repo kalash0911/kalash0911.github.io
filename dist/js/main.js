fab2308ea0c25e15f01151e51e523909f42c5520
kalash0911 <kalashnikov0911@gmail.com>
2021-03-06 17:34:10 +0200

    add pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,4 +7,18 @@ jQuery(function () {
 var greeting = function greeting() {
   return console.log("Hi!");
 };
+
+$(document).ready(function () {
+  $('.get-for-btn').click(function (event) {
+    event.preventDefault();
+    $('.overlay').toggleClass('overlay-visible');
+    $('.get-for').toggleClass('get-for-visible');
+  });
+});
+/* $(document).ready(function() {
+    $('.burger').click(function(event){
+        $('.nav-menu').toggleClass('nav-active');
+        $('.burger').toggleClass('burger-active');
+    });
+}); */
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@ jQuery(function() {
 });
  
 let greeting = () => console.log("Hi!");  
+
+$(document).ready(function() {
+    $('.get-for-btn').click(function(event){
+        event.preventDefault()
+        $('.overlay').toggleClass('overlay-visible');
+        $('.get-for').toggleClass('get-for-visible');
+    });
+});
+
+/* $(document).ready(function() {
+    $('.burger').click(function(event){
+        $('.nav-menu').toggleClass('nav-active');
+        $('.burger').toggleClass('burger-active');
+    });
+}); */
