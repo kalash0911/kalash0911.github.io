c239a85ee5911322a8707ac2fa8234a820b31dc9
kalash0911 <kalashnikov0911@gmail.com>
2021-03-30 03:28:39 +0300

    add sections

--- Alexander-Hobbs/src/js/main.js
+++ Alexander-Hobbs/src/js/main.js
@@ -3,7 +3,7 @@ jQuery(function() {
  
 new BeerSlider(document.getElementById('slider'));
 
-const swiper = new Swiper('.slider-cub', {
+new Swiper('.slider-cub', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -29,4 +29,50 @@ const swiper = new Swiper('.slider-cub', {
     loop: true,
     grabCursor: true,
     speed: 1000,
+});
+
+new Swiper('.slider-events', {
+    slidesPerView: 4,
+    freeMode: true,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
+
+new Swiper('.slider-img', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'flip',
+    slidesPerView: 1,
+    speed: 800,
+
+    thumbs: {
+        swiper: {
+            el: '.slider-text',
+            direction: 'vertical',
+            slidesPerView: 8,
+            freeMode: true,
+            mousewheel: true,
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        },
+    },
+});
+
+new Swiper('.slider-news', {
+    speed: 1000,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
