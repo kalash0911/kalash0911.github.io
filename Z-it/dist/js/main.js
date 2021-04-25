2099d78bfa868ebe5336cd91af65dda508a6bfa5
kalash0911 <kalashnikov0911@gmail.com>
2021-04-25 07:10:49 +0300

    add z-it

---
+++ Z-it/dist/js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var isMobile = {
+  Android: function Android() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function BlackBerry() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function iOS() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function Opera() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function Windows() {
+    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function any() {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+  }
+};
+
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+  var menuArrows = document.querySelectorAll('.menu__arrow');
+
+  if (menuArrows.length > 0) {
+    var _loop = function _loop(index) {
+      var menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle('_active');
+      });
+    };
+
+    for (var index = 0; index < menuArrows.length; index++) {
+      _loop(index);
+    }
+  }
+} else {
+  document.body.classList.add('_pc');
+}
+//# sourceMappingURL=main.js.map
