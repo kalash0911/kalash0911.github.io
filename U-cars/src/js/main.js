d243d817940edaf54c39a484bd34f79441885016
kalash0911 <kalashnikov0911@gmail.com>
2021-05-17 00:40:00 +0300

    psd

---
+++ U-cars/src/js/main.js
@@ -0,0 +1,42 @@
+let citiList = document.querySelector('.citi__list'),
+    citiToggle = document.querySelector('.citi'),
+    citiClose = document.querySelector('.list_close');
+
+    citiToggle.onclick = function() {
+        citiList.style.display = "block"
+    };
+
+    citiClose.onclick = function() {
+        citiList.style.display = "none"
+    };
+
+    window.onclick = function(e) {
+        if(e.target == citiList) {
+            citiList.style.display = "none"
+        }
+    };
+
+
+$(function(){
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: 'Предыдущий',
+        nextText: 'Следующий',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $("#datepicker").datepicker({
+        minDate: 0,
+	});
+});
