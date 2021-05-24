8ef2fa4bc4515f75a98b216a70debf3e2bbefede
kalash0911 <kalashnikov0911@gmail.com>
2021-05-25 00:41:12 +0300

    end

--- U-cars/dist/js/main.js
+++ U-cars/dist/js/main.js
@@ -1,23 +1,106 @@
 "use strict";
 
-var citiList = document.querySelector('.citi-list__ul'),
-    citiToggle = document.querySelector('.citi-list__title'),
-    citiClose = document.querySelector('.list_close');
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
-citiToggle.onclick = function () {
-  citiList.style.display = "block";
-};
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-citiClose.onclick = function () {
-  citiList.style.display = "none";
-};
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-window.onclick = function (e) {
-  if (e.target == citiList) {
-    citiList.style.display = "none";
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var isMobile = {
+  Android: function Android() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function BlackBerry() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function iOS() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function Opera() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function Windows() {
+    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function any() {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
   }
 };
 
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+  var menuArrows = document.querySelectorAll('.menu__arrow');
+
+  if (menuArrows.length > 0) {
+    var _loop = function _loop(index) {
+      var menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle('_active');
+      });
+    };
+
+    for (var index = 0; index < menuArrows.length; index++) {
+      _loop(index);
+    }
+  }
+} else {
+  document.body.classList.add('_pc');
+}
+
+var menuBurger = document.querySelector('.menu__burger');
+
+if (menuBurger) {
+  var menuBody = document.querySelector('.menu__body');
+  menuBurger.addEventListener("click", function (e) {
+    document.body.classList.toggle('body_lock');
+    menuBurger.classList.toggle('menu__burger_active');
+    menuBody.classList.toggle('menu__body_active');
+  });
+}
+
+var citiList = _toConsumableArray(document.querySelectorAll('.citi-list__ul')),
+    citiesToggle = _toConsumableArray(document.querySelectorAll('.citi-list__title')),
+    citiClose = document.querySelector('.list_close');
+
+citiesToggle.forEach(function (el) {
+  el.onclick = function (e) {
+    e.preventDefault();
+    var parent = e.target.parentElement;
+    var citiList = parent.querySelector('.citi-list__ul');
+    citiList.classList.toggle('citi-list__ul_active');
+  };
+});
+citiList.forEach(function (el) {
+  el.onclick = function (e) {
+    e.preventDefault();
+    el.classList.remove('citi-list__ul_active');
+  };
+});
+
+var clockList = _toConsumableArray(document.querySelectorAll('.clock-list__ul')),
+    clocksToggle = _toConsumableArray(document.querySelectorAll('.clock-list__title')),
+    clockClose = document.querySelector('.clock-list_close');
+
+clocksToggle.forEach(function (el) {
+  el.onclick = function (e) {
+    e.preventDefault();
+    var parent = e.target.parentElement;
+    var clockList = parent.querySelector('.clock-list__ul');
+    clockList.classList.toggle('clock-list__ul_active');
+  };
+});
+clockList.forEach(function (el) {
+  el.onclick = function (e) {
+    e.preventDefault();
+    el.classList.remove('clock-list__ul_active');
+  };
+});
 $(function () {
   $.datepicker.regional['ru'] = {
     closeText: 'Закрыть',
@@ -38,6 +121,29 @@ $(function () {
   };
   $.datepicker.setDefaults($.datepicker.regional['ru']);
   $("#datepicker").datepicker({
+    minDate: 0
+  });
+});
+$(function () {
+  $.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: 'Предыдущий',
+    nextText: 'Следующий',
+    currentText: 'Сегодня',
+    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+    dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+    dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    weekHeader: 'Не',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''
+  };
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+  $("#datepicker2").datepicker({
     minDate: 0
   });
 });
