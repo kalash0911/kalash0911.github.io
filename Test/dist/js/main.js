017cb69b70c262e70d1274f32a79c36c6e236f53
Yevhen <73849653+kalash0911@users.noreply.github.com>
2021-08-26 17:58:24 +0300

    test

--- Test/dist/js/main.js
+++ Test/dist/js/main.js
@@ -1,10 +1,58 @@
 "use strict";
 
-var itemImg = document.querySelectorAll('.main__item-img');
+var isMobile = {
+  Android: function Android() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function BlackBerry() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function iOS() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function Opera() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function Windows() {
+    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function any() {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+  }
+};
+
+window.onload = function () {
+  document.addEventListener("click", documentActions);
+
+  function documentActions(e) {
+    var targetElement = e.target;
+
+    if (window.innerWidth < 5000 && isMobile.any()) {
+      if (targetElement.classList.contains('btn-arrow')) {
+        targetElement.closest('.menu__item').classList.toggle('_hover');
+      }
+    }
+  }
+};
+
+var burger = document.querySelector(".burger");
+
+if (burger) {
+  var menuBody = document.querySelector(".menu");
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    burger.classList.toggle("burger_active");
+    menuBody.classList.toggle("menu_active");
+  });
+}
+/* Modal popUp img */
+
+
+var itemImg = document.querySelectorAll('.gallery__item-img');
 var modal = document.querySelector('.modal-img');
 var modalImg = modal.querySelector('img');
 var overflowImg = document.querySelector('.modal-img__overflow');
-var closeBtnImg = document.querySelector('.modal-img__close');
+var closeBtnImg = document.querySelector('.modal-btn-close');
 
 function showImg(e) {
   document.body.classList.add("body_lock");
@@ -45,4 +93,5 @@ if (modalActive) {
 }
 
 ;
+/* ///// */
 //# sourceMappingURL=main.js.map
--- Test/src/js/main.js
+++ Test/src/js/main.js
@@ -1,8 +1,59 @@
-const itemImg = document.querySelectorAll('.main__item-img');
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+window.onload = function () {
+    document.addEventListener("click", documentActions);
+
+    function documentActions(e) {
+        const targetElement = e.target;
+        if (window.innerWidth < 5000 && isMobile.any()) {
+            if (targetElement.classList.contains('btn-arrow')) {
+                targetElement.closest('.menu__item').classList.toggle('_hover')
+            }
+        }
+    }
+};
+
+const burger = document.querySelector(".burger");
+if (burger) {
+  const menuBody = document.querySelector(".menu");
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    burger.classList.toggle("burger_active");
+    menuBody.classList.toggle("menu_active");
+  });
+}
+
+/* Modal popUp img */
+
+const itemImg = document.querySelectorAll('.gallery__item-img');
 const modal =  document.querySelector('.modal-img');
 const modalImg = modal.querySelector('img');
 const overflowImg = document.querySelector('.modal-img__overflow');
-const closeBtnImg  = document.querySelector('.modal-img__close');
+const closeBtnImg  = document.querySelector('.modal-btn-close');
 
 function showImg(e){
     document.body.classList.add("body_lock");
@@ -39,3 +90,5 @@ if (modalActive) {
         e.preventDefault();
     });
 };
+
+/* ///// */
