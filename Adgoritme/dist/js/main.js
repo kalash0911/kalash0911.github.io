35308c753713ac74f6a936664c76b67a70f7ee7e
kalash0911 <kalashnikov0911@gmail.com>
2021-10-20 17:48:43 +0300

    add new pages

--- Adgoritme/dist/js/main.js
+++ Adgoritme/dist/js/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var isMobile = {
   Android: function Android() {
     return navigator.userAgent.match(/Android/i);
@@ -20,25 +26,11 @@ var isMobile = {
     return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
   }
 };
-
-window.onload = function () {
-  document.addEventListener("click", documentActions);
-
-  function documentActions(e) {
-    var targetElement = e.target;
-
-    if (window.innerWidth < 5000 && isMobile.any()) {
-      if (targetElement.classList.contains('btn-arrow')) {
-        targetElement.closest('.menu__item').classList.toggle('_hover');
-      }
-    }
-  }
-};
-
 var burger = document.querySelector(".burger");
+var menuBody = document.querySelector(".menu");
+var linkClose = document.querySelectorAll(".link-close");
 
 if (burger) {
-  var menuBody = document.querySelector(".menu");
   burger.addEventListener("click", function (e) {
     document.body.classList.toggle("body_lock");
     document.body.classList.toggle("active");
@@ -46,4 +38,73 @@ if (burger) {
     menuBody.classList.toggle("menu_active");
   });
 }
+
+;
+
+if (linkClose.length) {
+  for (var i = 0; i < linkClose.length; ++i) {
+    linkClose[i].addEventListener("click", function (e) {
+      document.body.classList.remove("body_lock");
+      document.body.classList.remove("active");
+      burger.classList.remove("burger_active");
+      menuBody.classList.remove("menu_active");
+    });
+  }
+}
+
+;
+
+function initSliders(selector, width, obj) {
+  var init = _objectSpread({
+    infinite: false,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  }, obj);
+
+  $(function () {
+    var win = $(window);
+    var slider = $(selector);
+    win.on("load resize", function () {
+      if (win.width() <= width) {
+        slider.not(".slick-initialized").slick(init);
+      } else if (slider.hasClass("slick-initialized")) {
+        slider.slick("unslick");
+      }
+    });
+  });
+}
+
+;
+initSliders(".partners-slider", 99999, {
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  speed: 700,
+  dots: false,
+  arrows: true,
+  adaptiveHeight: true,
+  touchThreshold: 50,
+  nextArrow: document.querySelector('.arrow-next'),
+  prevArrow: document.querySelector('.arrow-prev'),
+  responsive: [{
+    breakpoint: 1300,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3
+    }
+  }, {
+    breakpoint: 960,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  }, {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }]
+});
 //# sourceMappingURL=main.js.map
--- Adgoritme/src/js/main.js
+++ Adgoritme/src/js/main.js
@@ -24,26 +24,86 @@ var isMobile = {
     }
 };
 
-window.onload = function () {
-    document.addEventListener("click", documentActions);
-
-    function documentActions(e) {
-        const targetElement = e.target;
-        if (window.innerWidth < 5000 && isMobile.any()) {
-            if (targetElement.classList.contains('btn-arrow')) {
-                targetElement.closest('.menu__item').classList.toggle('_hover')
-            }
-        }
-    }
-};
-
 const burger = document.querySelector(".burger");
+const menuBody = document.querySelector(".menu");
+const linkClose = document.querySelectorAll(".link-close");
 if (burger) {
-  const menuBody = document.querySelector(".menu");
   burger.addEventListener("click", function (e) {
     document.body.classList.toggle("body_lock");
     document.body.classList.toggle("active");
     burger.classList.toggle("burger_active");
     menuBody.classList.toggle("menu_active");
   });
-}
+};
+
+if (linkClose.length) {
+    for(var i =0;i<linkClose.length;++i){
+        linkClose[i].addEventListener("click", function (e) {
+            document.body.classList.remove("body_lock");
+            document.body.classList.remove("active");
+            burger.classList.remove("burger_active");
+            menuBody.classList.remove("menu_active");
+        })
+    }
+};
+
+
+
+function initSliders(selector, width, obj) {
+    const init = {
+      infinite: false,
+      cssEase: "linear",
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      ...obj,
+    };
+  
+    $(() => {
+      const win = $(window);
+      const slider = $(selector);
+  
+      win.on("load resize", () => {
+        if (win.width() <= width) {
+          slider.not(".slick-initialized").slick(init);
+        } else if (slider.hasClass("slick-initialized")) {
+          slider.slick("unslick");
+        }
+      });
+    });
+};
+
+initSliders(".partners-slider", 99999, {
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 700,
+    dots: false,
+    arrows: true,
+    adaptiveHeight: true,
+    touchThreshold: 50,
+    nextArrow: document.querySelector('.arrow-next'),
+    prevArrow: document.querySelector('.arrow-prev'),
+    responsive: [
+        {
+        breakpoint: 1300,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            },
+        },
+        {
+        breakpoint: 960,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+        breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
