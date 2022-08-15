bfdb0c915eb2582728c4d2228b41bd62b2598d47
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-08-15 11:56:56 +0300

    added reviews for service page1

--- Planet/src/js/main.js
+++ Planet/src/js/main.js
@@ -96,3 +96,40 @@ tabsBtn.forEach(function(item) {
         }
     });
 });
+
+// Swiper:
+
+function destroySlidersOnResize(selector, width, obj, moreThan) {
+  const init = {
+    ...obj,
+  };
+
+  const win = window;
+  const sliderSelector = document.querySelector(selector);
+  let swiper = new Swiper(selector, init);
+
+  const toggleInit = () => {
+    const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
+    if (neededWidth) {
+      if (!sliderSelector.classList.contains("swiper-initialized")) {
+        swiper = new Swiper(selector, init);
+      }
+    } else if (sliderSelector.classList.contains("swiper-initialized")) {
+      swiper.destroy();
+    }
+  };
+
+  ["load", "resize"].forEach((evt) =>
+    win.addEventListener(evt, toggleInit, false)
+  );
+}
+
+destroySlidersOnResize(".reviews-slider", 99999, {
+  spaceBetween: 20,
+  autoHeight: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
