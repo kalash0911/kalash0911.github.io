3056c3694ca6d266418a085e2d29d9cdfa2a2a96
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-08-30 20:16:56 +0300

    added header

--- MAXRatings/src/js/main.js
+++ MAXRatings/src/js/main.js
@@ -1,3 +1,33 @@
+/* header logick */
+
+const burger = document.querySelector(".burger");
+const menuBody = document.querySelector(".menu-wrap");
+const linkClose = document.querySelectorAll(".link-close");
+if (burger) {
+burger.addEventListener("click", function (e) {
+  document.body.classList.toggle("body_lock");
+  document.body.classList.toggle("active");
+  if(burger.classList.contains('burger_active')) {
+      burger.classList.add('burger_finish');
+      burger.classList.remove('burger_active');
+    } else {
+      burger.classList.add('burger_active');
+      burger.classList.remove('burger_finish');
+    }
+});
+};
+
+if (linkClose.length) {
+  for(var i =0;i<linkClose.length;++i){
+      linkClose[i].addEventListener("click", function (e) {
+          document.body.classList.remove("body_lock");
+          document.body.classList.remove("active");
+          burger.classList.remove("burger_active");
+          burger.classList.add('burger_finish');
+      })
+  }
+};
+
 // Swiper:
 
 function destroySlidersOnResize(selector, width, obj, moreThan) {
