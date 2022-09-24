73cc5fd307d06ffbcd5ec3e1d5d08e4740deaed5
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-09-25 00:18:46 +0300

    fix links js

--- Planet/src/js/constants/link.js
+++ Planet/src/js/constants/link.js
@@ -1,6 +1,4 @@
-export const PLANET_HOST = window.DEVELOPMENT_MODE
-  ? window.location.origin
-  : "https://planetaemailsender.azurewebsites.net";
+export const PLANET_HOST = "https://planetaemailsender.azurewebsites.net";
 
 export const PLANET_ENDPOINT = `${PLANET_HOST}/result`;
 export const PLANET_ENDPOINT_CONTACT = `${PLANET_HOST}/contact`;
