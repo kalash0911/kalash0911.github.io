478da300057c64d449752b1173a5e3176ac92dd4
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-09-27 15:54:04 +0300

    added html header

--- MeatingBurgers/dist/js/main.js
+++ MeatingBurgers/dist/js/main.js
@@ -1,2 +1,37 @@
 "use strict";
+
+/* header logick */
+var burger = document.querySelector(".burger");
+var menuBody = document.querySelector(".menu-wrap");
+var linkClose = document.querySelectorAll(".link-close");
+
+if (burger) {
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    document.body.classList.toggle("active");
+
+    if (burger.classList.contains('burger_active')) {
+      burger.classList.add('burger_finish');
+      burger.classList.remove('burger_active');
+    } else {
+      burger.classList.add('burger_active');
+      burger.classList.remove('burger_finish');
+    }
+  });
+}
+
+;
+
+if (linkClose.length) {
+  for (var i = 0; i < linkClose.length; ++i) {
+    linkClose[i].addEventListener("click", function (e) {
+      document.body.classList.remove("body_lock");
+      document.body.classList.remove("active");
+      burger.classList.remove("burger_active");
+      burger.classList.add('burger_finish');
+    });
+  }
+}
+
+;
 //# sourceMappingURL=main.js.map
