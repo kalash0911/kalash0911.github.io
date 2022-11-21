745e1adc397d8273458d83b1988b330ebcb193df
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-11-21 21:52:33 +0200

    delete same projects and added new project

--- U-cars/gulp/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-  
-const del = require('del')
-
-module.exports = function clean(cb) {
-  return del('dist').then(() => {
-    cb()
-  })
-}
--- Z-it/gulp/tasks/fonts.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-
-
-module.exports = function imgMin() {
-    return gulp.src('src/fonts/*')
-    .pipe(gulp.dest('dist/fonts'))
-}
-
