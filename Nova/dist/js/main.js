2345cf1b0972abe5bcb032739dfc0f3882c0d4fc
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-12-08 22:02:38 +0200

    added header

--- Nova/dist/js/main.js
+++ Nova/dist/js/main.js
@@ -1,2 +1,14 @@
 "use strict";
+
+var burger = document.querySelector(".burger");
+var menuBody = document.querySelector(".menu-wrap");
+
+if (burger) {
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    document.body.classList.toggle("active");
+  });
+}
+
+;
 //# sourceMappingURL=main.js.map
--- Nova/src/js/main.js
+++ Nova/src/js/main.js
@@ -1,1 +1,9 @@
-  
+const burger = document.querySelector(".burger");
+const menuBody = document.querySelector(".menu-wrap");
+
+if (burger) {
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    document.body.classList.toggle("active");
+  });
+};
