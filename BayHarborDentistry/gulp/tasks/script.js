1c98d5410efc9fba2bd0335dfbc7595fd7a1b74c
yzhura <zhuravlev.yaroslav@gmail.com>
2023-02-07 00:18:05 +0200

    Update script.js

--- BayHarborDentistry/gulp/tasks/script.js
+++ BayHarborDentistry/gulp/tasks/script.js
@@ -1,17 +1,18 @@
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const rigger = require('gulp-rigger');
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const babel = require("gulp-babel");
+const sourcemaps = require("gulp-sourcemaps");
+const rigger = require("gulp-rigger");
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
 
 module.exports = function script() {
-  return gulp.src('src/js/*.js')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(rigger())
-    .pipe(babel({
-      presets: ['@babel/env']
-     }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/js'));
+  return browserify(["src/js/main.js"])
+    .transform(babelify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest("dist/js"))
 };
