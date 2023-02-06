f9b4617d406df21332b6a5fa85285df4e8b0e02d
yzhura <zhuravlev.yaroslav@gmail.com>
2023-02-06 20:07:45 +0200

    add simple contact form validation

--- BayHarborDentistry/src/js/main.js
+++ BayHarborDentistry/src/js/main.js
@@ -10,7 +10,13 @@ initVideoPopup();
 initAccordion();
 initActiveSubMenu();
 initContactFormPopup();
+initContactForm();
 
+function initContactForm() {
+  Foundation.Abide.defaults.patterns['time'] = /^(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9])$/;
+  Foundation.Abide.defaults.patterns['tel'] = /^\d{10}$/;
+  new Foundation.Abide($('.contactForm'));
+}
 
 // ContactForm popup
 function initContactFormPopup() {
