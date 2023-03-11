3034079ab92b3276da7bdc1ce7a8b0a68cf0f9f6
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-03-11 23:16:37 +0200

    added new project

---
+++ CVOE/dist/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// Swiper:
+function destroySlidersOnResize(selector, width, obj, moreThan) {
+  var init = _objectSpread({}, obj);
+
+  var win = window;
+  var sliderSelector = document.querySelector(selector);
+  var swiper = new Swiper(selector, init);
+
+  var toggleInit = function toggleInit() {
+    var neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width;
+
+    if (neededWidth) {
+      if (!sliderSelector.classList.contains("swiper-initialized")) {
+        swiper = new Swiper(selector, init);
+      }
+    } else if (sliderSelector.classList.contains("swiper-initialized")) {
+      swiper.destroy();
+    }
+  };
+
+  ["load", "resize"].forEach(function (evt) {
+    return win.addEventListener(evt, toggleInit, false);
+  });
+}
+
+destroySlidersOnResize(".me-slider", 960, {
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+//# sourceMappingURL=main.js.map
