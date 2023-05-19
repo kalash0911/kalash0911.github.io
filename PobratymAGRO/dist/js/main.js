4ec83773e3d47f7c15fae98d082a8a70fc906bf7
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-05-19 16:56:07 +0300

    added footer and header

--- PobratymAGRO/dist/js/main.js
+++ PobratymAGRO/dist/js/main.js
@@ -6,7 +6,41 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// Swiper:
+/* header logick */
+var burger = document.querySelector(".burger");
+var menuBody = document.querySelector(".menu-wrap");
+var linkClose = document.querySelectorAll(".link-close");
+
+if (burger) {
+  burger.addEventListener("click", function (e) {
+    document.body.classList.toggle("body_lock");
+    document.body.classList.toggle("active");
+
+    if (burger.classList.contains('burger_active')) {
+      burger.classList.add('burger_finish');
+      burger.classList.remove('burger_active');
+    } else {
+      burger.classList.add('burger_active');
+      burger.classList.remove('burger_finish');
+    }
+  });
+}
+
+;
+
+if (linkClose.length) {
+  for (var i = 0; i < linkClose.length; ++i) {
+    linkClose[i].addEventListener("click", function (e) {
+      document.body.classList.remove("body_lock");
+      document.body.classList.remove("active");
+      burger.classList.remove("burger_active");
+      burger.classList.add('burger_finish');
+    });
+  }
+}
+
+; // Swiper:
+
 function destroySlidersOnResize(selector, width, obj, moreThan) {
   var init = _objectSpread({}, obj);
 
