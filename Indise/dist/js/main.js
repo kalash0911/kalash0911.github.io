38b2846d09419f1f4e8bb470c6dbaff36e390bea
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-07-03 15:41:36 +0300

    added some styles

--- Indise/dist/js/main.js
+++ Indise/dist/js/main.js
@@ -10,6 +10,7 @@ var burger = document.querySelector(".burger");
 var menuBody = document.querySelector(".menu-wrap");
 var linkClose = document.querySelectorAll(".link-close");
 var overflow = document.querySelector(".overflow");
+var gifs = Gifffer();
 var winTriggersMethods = ["resize", "load"];
 var prevWidth = window.innerWidth;
 winTriggersMethods.forEach(function (method) {
@@ -124,18 +125,19 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
   ["load", "resize"].forEach(function (evt) {
     return win.addEventListener(evt, toggleInit, false);
   });
+  return swiper;
 }
 
 var menuSteps = ['Write a detailed description of the design you would like to create', 'Select your desired square footage', 'Choose one interior design style from our catalog', 'As needed, you can upload a reference image directly from your iPhone gallery', 'Just 60 seconds of patience…', '4 results are ready. Edit them or upscale for higher resolution', "Here's your interior design, delivered in under 90 seconds"];
-destroySlidersOnResize(".stepSlider", 9999999, {
+var prevTime = 3000;
+var animPhoneSlider = destroySlidersOnResize(".stepSlider", 9999999, {
   spaceBetween: 20,
   effect: "fade",
   speed: 1200,
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false
-    /* stopOnLastSlide: true, */
-
+    disableOnInteraction: false,
+    stopOnLastSlide: true
   },
   // autoplay: false,
   pagination: {
@@ -147,17 +149,39 @@ destroySlidersOnResize(".stepSlider", 9999999, {
   },
   on: {
     autoplayTimeLeft: function autoplayTimeLeft(swiper, time, progress) {
+      console.log('time: ', time);
       var current = swiper.activeIndex + 1;
       var max = swiper.slides.length;
       var currentPercents = current / max + -progress / max;
       var clockArrowDeg = currentPercents * 360;
       var arrowEl = document.querySelector('.clock-arrow');
       var progressCircle = document.querySelector(".autoplay-progress svg");
-      arrowEl.style.transform = "rotate(".concat(clockArrowDeg, "deg)");
-      progressCircle.style.setProperty("--progress", currentPercents - 1 / 170); // progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+
+      if (time > 0) {
+        if (prevTime - time > 313) {
+          arrowEl.style.transform = "rotate(".concat(clockArrowDeg, "deg)");
+          progressCircle.style.setProperty("--progress", currentPercents - 1 / 170);
+          prevTime = time;
+        } else if (time < 100) {
+          arrowEl.style.transform = "rotate(".concat(clockArrowDeg, "deg)");
+          progressCircle.style.setProperty("--progress", currentPercents - 1 / 170);
+        }
+
+        if (time === 3000) prevTime = time;
+      }
+    },
+    afterInit: function afterInit(swiper) {
+      var currentGif = swiper.visibleSlides[0].querySelector('.gif');
+      currentGif.click();
+    },
+    activeIndexChange: function activeIndexChange(swiper) {
+      var currentGif = swiper.visibleSlides[0].querySelector('.gif');
+      currentGif.click();
+      swiper.slides[swiper.previousIndex].querySelector('.gif').click();
     }
   }
 });
+animPhoneSlider.autoplay.stop();
 
 function phoneAnimation() {
   var secondSection = document.querySelector('.sec-section');
@@ -169,7 +193,8 @@ function phoneAnimation() {
   if (!secondSection || !phone) return;
   var sectionRect = secondSection.getBoundingClientRect();
   var fromY = sectionRect.top + 230;
-  var firstSlide = document.querySelectorAll(".swiper-slide img")[0];
+  var firstSlide = document.querySelectorAll(".swiper-slide .gif")[0];
+  console.log('firstSlide: ', firstSlide);
   firstSlide.classList.add('hidden');
   gsap.fromTo(phone, {
     x: 0,
@@ -188,6 +213,8 @@ function phoneAnimation() {
       onLeave: function onLeave() {
         phone.classList.add('d-none');
         firstSlide.classList.remove('hidden');
+        animPhoneSlider.destroy();
+        window.dispatchEvent(new Event('resize'));
       }
     }
   });
