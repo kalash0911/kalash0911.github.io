8dfd158dc619b98290ce48b91d94897003b9cded
yzhura <zhuravlev.yaroslav@gmail.com>
2023-08-10 23:33:49 +0300

    sticky anim structure

--- Ryan/dist/js/main.js
+++ Ryan/dist/js/main.js
@@ -1,12 +1,25 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// Swiper:
+initStickyPhone(); // Swiper:
+
 function destroySlidersOnResize(selector, width, obj, moreThan) {
   var init = _objectSpread({}, obj);
 
@@ -37,4 +50,70 @@ destroySlidersOnResize(".me-slider", 960, {
     el: ".swiper-pagination"
   }
 });
+
+function initStickyPhone() {
+  var startSection = document.querySelector(".phone-section");
+  var phonesWrapper = startSection.querySelector('.sticky-phones-wrapper');
+  var phoneContent = phonesWrapper.querySelector('.sticky-phones-content');
+  var phoneImgs = phoneContent.querySelectorAll('img');
+  var steps = document.querySelectorAll('.steps li');
+
+  var stepsReact = _toConsumableArray(steps).reduce(function (prev, cur, ind) {
+    var stepRect = cur.getBoundingClientRect();
+    prev["".concat(ind)] = stepRect;
+    return prev;
+  }, {});
+
+  var sectionRect = startSection.getBoundingClientRect();
+  phoneImgs.forEach(function (img, ind) {
+    return img.style.zIndex = "".concat(ind);
+  });
+  if (!phonesWrapper || !startSection || !steps) return;
+  var phoneWrapDesctination = sectionRect.height - stepsReact[steps.length - 1].height - 50; // Phone wrapper scroll anim
+
+  gsap.fromTo(phonesWrapper, {
+    x: 5,
+    y: -stepsReact[0].height / 2,
+    rotation: 3,
+    skewX: -2
+  }, {
+    x: -5,
+    y: phoneWrapDesctination,
+    rotation: -3,
+    skewX: 2,
+    scrollTrigger: {
+      trigger: startSection,
+      start: "top 40%",
+      end: "bottom",
+      scrub: 0.5 // markers: true,
+
+    }
+  });
+  var phoneContentHeight = phoneContent.getBoundingClientRect().height; // Content images scroll anim
+
+  phoneImgs.forEach(function (img, ind) {
+    if (ind > 0) {
+      gsap.fromTo(img, {
+        x: 0,
+        y: -phoneContentHeight,
+        rotation: 0
+      }, {
+        x: 0,
+        y: 0,
+        rotation: 0,
+        scrollTrigger: {
+          trigger: steps[ind],
+          start: "20% 80%-".concat(phoneContentHeight),
+          end: "bottom-=100px 85%",
+          scrub: 1,
+          // markers: true,
+          onUpdate: function onUpdate(self) {
+            var filterValue = self.progress.toFixed(3) * 20;
+            phoneImgs[ind - 1].style.filter = "blur(".concat(filterValue, "px)");
+          }
+        }
+      });
+    }
+  });
+}
 //# sourceMappingURL=main.js.map
--- Ryan/src/js/main.js
+++ Ryan/src/js/main.js
@@ -1,3 +1,6 @@
+
+initStickyPhone();
+
 // Swiper:
 
 function destroySlidersOnResize(selector, width, obj, moreThan) {
@@ -34,3 +37,78 @@ destroySlidersOnResize(".me-slider", 960, {
     el: ".swiper-pagination",
   },
 });
+
+function initStickyPhone() {
+  const startSection = document.querySelector(".phone-section");
+  const phonesWrapper = startSection.querySelector('.sticky-phones-wrapper');
+  const phoneContent = phonesWrapper.querySelector('.sticky-phones-content')
+  const phoneImgs = phoneContent.querySelectorAll('img');
+  const steps = document.querySelectorAll('.steps li');
+  const stepsReact = [...steps].reduce((prev, cur, ind) => {
+    const stepRect = cur.getBoundingClientRect();
+    prev[`${ind}`] = stepRect;
+    return prev;
+  }, {})
+  const sectionRect = startSection.getBoundingClientRect();
+  phoneImgs.forEach((img, ind) => img.style.zIndex = `${ind}`)
+
+  if(!phonesWrapper || !startSection || !steps) return;
+
+  const phoneWrapDesctination = sectionRect.height - stepsReact[steps.length - 1].height - 50;
+
+  // Phone wrapper scroll anim
+  gsap.fromTo(
+    phonesWrapper,
+    {
+      x: 5,
+      y: -stepsReact[0].height / 2,
+      rotation: 3,
+      skewX: -2,
+    },
+    {
+      x: -5,
+      y: phoneWrapDesctination,
+      rotation: -3,
+      skewX: 2,
+      scrollTrigger: {
+        trigger: startSection,
+        start: `top 40%`,
+        end: `bottom`,
+        scrub: 0.5,
+        // markers: true,
+      },
+    }
+  );
+
+  const phoneContentHeight = phoneContent.getBoundingClientRect().height;
+
+  // Content images scroll anim
+  phoneImgs.forEach((img, ind) => {
+    if(ind > 0) {
+      gsap.fromTo(
+        img,
+        {
+          x: 0,
+          y: -phoneContentHeight,
+          rotation: 0,
+        },
+        {
+          x: 0,
+          y: 0,
+          rotation: 0,
+          scrollTrigger: {
+            trigger: steps[ind],
+            start: `20% 80%-${phoneContentHeight}`,
+            end: `bottom-=100px 85%`,
+            scrub: 1,
+            // markers: true,
+            onUpdate: (self) => {
+              const filterValue = self.progress.toFixed(3) * 20;
+              phoneImgs[ind-1].style.filter = `blur(${filterValue}px)`;
+            }
+          },
+        }
+      )
+    }
+  })
+}
