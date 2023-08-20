dacc1d5e5de6853e4ad73d9b6162a91e2ce67290
yzhura <zhuravlev.yaroslav@gmail.com>
2023-08-20 18:14:06 +0300

    update kids anim

--- Ryan/dist/js/main.js
+++ Ryan/dist/js/main.js
@@ -20,11 +20,11 @@ window.addEventListener("load", function (event) {
 function initStickyPhone() {
   var startSection = document.querySelector(".phone-section");
   var phonesWrapper = startSection.querySelector('.sticky-phones-wrapper');
-  var phoneContent = phonesWrapper.querySelector('.sticky-phones-content');
-  var kidsWrapper = startSection.querySelector('.sticky-kids-wrapper');
-  var kidsContent = startSection.querySelector('.sticky-kids-content');
-  var phoneImgs = phoneContent.querySelectorAll('img');
-  var kidsElements = kidsContent.querySelectorAll('[id*=kids_anim]');
+  var phoneContent = phonesWrapper.querySelector('.sticky-phones-content'); // const kidsWrapper = startSection.querySelector('.sticky-kids-wrapper');
+  // const kidsContent = startSection.querySelector('.sticky-kids-content');
+
+  var phoneImgs = phoneContent.querySelectorAll('img'); // const kidsElements = kidsContent.querySelectorAll('[id*=kids_anim]');
+
   var steps = document.querySelectorAll('.steps li');
 
   var stepsReact = _toConsumableArray(steps).reduce(function (prev, cur, ind) {
@@ -36,13 +36,10 @@ function initStickyPhone() {
   var sectionRect = startSection.getBoundingClientRect();
   phoneImgs.forEach(function (img, ind) {
     return img.style.zIndex = "".concat(ind);
-  });
-  kidsElements.forEach(function (el, ind) {
-    return el.style.zIndex = "".concat(ind);
-  });
+  }); // kidsElements.forEach((el, ind) => el.style.zIndex = `${ind}`);
+
   var phoneWrapDesctination = sectionRect.height - stepsReact[0].height + 280;
-  var phoneContentHeight = phoneContent.getBoundingClientRect().height;
-  var kidsContentHeight = kidsContent.getBoundingClientRect().height; // Phone wrapper scroll anim
+  var phoneContentHeight = phoneContent.getBoundingClientRect().height; // Phone wrapper scroll anim
 
   gsap.fromTo(phonesWrapper, {
     x: 5,
@@ -53,31 +50,37 @@ function initStickyPhone() {
     rotation: -3,
     skewX: 2,
     scrollTrigger: {
-      trigger: phonesWrapper,
-      start: "".concat(stepsReact[0].height, "-=250px center"),
+      trigger: steps[0].querySelector('.title'),
+      start: "50% center",
       end: "".concat(phoneWrapDesctination, " center"),
       scrub: 0.5,
       // markers: true,
-      pin: phonesWrapper
-    }
-  }); //Kids wrapper scroll anim
-
-  gsap.fromTo(kidsWrapper, {
-    x: 0
-  }, {
-    x: 0,
-    scrollTrigger: {
-      trigger: phonesWrapper,
-      start: "".concat(stepsReact[0].height, "-=250px center"),
-      end: "".concat(phoneWrapDesctination, " center"),
-      scrub: 0.5,
-      // markers: true,
-      pin: kidsWrapper,
-      onEnter: function onEnter() {
-        kidsAnimation[0].play();
+      pin: phonesWrapper,
+      onEnter: function onEnter() {// kidsAnimation[0].play();
       }
     }
-  }); // Content images scroll anim
+  }); //Kids wrapper scroll anim
+  // gsap.fromTo(
+  //   kidsWrapper,
+  //   {
+  //     x: 0,
+  //   },
+  //   {
+  //     x: 0,
+  //     scrollTrigger: {
+  //       trigger: phonesWrapper,
+  //       start: `${stepsReact[0].height}-=250px center`,
+  //       end: `${phoneWrapDesctination} center`,
+  //       scrub: 0.5,
+  //       // markers: true,
+  //       pin: kidsWrapper,
+  //       onEnter: () => {
+  //         kidsAnimation[0].play()
+  //       }
+  //     },
+  //   }
+  // );
+  // Content images scroll anim
 
   phoneImgs.forEach(function (img, ind) {
     if (ind > 0) {
@@ -91,51 +94,68 @@ function initStickyPhone() {
         rotation: 0,
         scrollTrigger: {
           trigger: steps[ind],
-          start: "75% 80%-".concat(phoneContentHeight),
-          end: "bottom+=10% 85%",
+          start: "65% 80%-".concat(phoneContentHeight),
+          end: "bottom+=20% 85%",
           scrub: 1,
           // markers: true,
           onUpdate: function onUpdate(self) {
             var filterValue = self.progress.toFixed(3) * 20;
             phoneImgs[ind - 1].style.filter = "blur(".concat(filterValue, "px)");
+
+            if (self.progress < 0.1) {
+              kidsAnimation[ind].pause();
+            } else {
+              if (kidsAnimation[ind].isPaused) {
+                kidsAnimation[ind].play();
+              }
+            }
+          },
+          onLeave: function onLeave(self) {
+            kidsAnimation[ind].play();
+            kidsAnimation[ind - 1].pause();
+          },
+          onEnterBack: function onEnterBack() {
+            kidsAnimation[ind - 1].play();
           }
         }
       });
     }
   }); // Kids anim scroll sticky
-
-  kidsElements.forEach(function (el, ind) {
-    if (ind > 0) {
-      gsap.fromTo(el, {
-        x: 0,
-        y: -500,
-        rotation: 0
-      }, {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        scrollTrigger: {
-          trigger: steps[ind],
-          start: "75% 80%-".concat(500),
-          end: "bottom+=10% 85%",
-          scrub: 1,
-          // markers: true,
-          onUpdate: function onUpdate(self) {
-            var filterValue = self.progress.toFixed(3) * 20;
-            kidsElements[ind - 1].style.filter = "blur(".concat(filterValue, "px)");
-            kidsElements[ind - 1].style.opacity = "".concat(1 - self.progress);
-
-            if (self.progress !== 1) {
-              kidsAnimation[ind].pause();
-            }
-          },
-          onLeave: function onLeave(self) {
-            kidsAnimation[ind].play();
-          }
-        }
-      });
-    }
-  });
+  //  kidsElements.forEach((el, ind) => {
+  //   if (ind > 0) {
+  //     gsap.fromTo(
+  //       el,
+  //       {
+  //         x: 0,
+  //         y: -500,
+  //         rotation: 0,
+  //       },
+  //       {
+  //         x: 0,
+  //         y: 0,
+  //         rotation: 0,
+  //         scrollTrigger: {
+  //           trigger: steps[ind],
+  //           start: `75% 80%-${500}`,
+  //           end: `bottom+=10% 85%`,
+  //           scrub: 1,
+  //           // markers: true,
+  //           onUpdate: (self) => {
+  //             const filterValue = self.progress.toFixed(3) * 20;
+  //             kidsElements[ind - 1].style.filter = `blur(${filterValue}px)`;
+  //             kidsElements[ind - 1].style.opacity = `${1 - self.progress}`;
+  //             if(self.progress !== 1) {
+  //               kidsAnimation[ind].pause();
+  //             }
+  //           },
+  //           onLeave: (self) => {
+  //             kidsAnimation[ind].play();
+  //           },
+  //         },
+  //       }
+  //     )
+  //   }
+  // });
 }
 /* ------------------- card section carousel ----------------*/
 
@@ -253,4 +273,23 @@ var kidsAnimation = new Array(5).fill('kids_anim').map(function (elem, ind, arr)
   });
   return anim;
 });
+
+function callback(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      kidsAnimation[0].play();
+    }
+  });
+}
+
+function createObserver(target, callback) {
+  var options = {
+    root: null,
+    threshold: 0
+  };
+  var observer = new IntersectionObserver(callback, options);
+  observer.observe(target);
+}
+
+createObserver(document.querySelector('#kids_anim_1'), callback);
 //# sourceMappingURL=main.js.map
--- Ryan/src/js/main.js
+++ Ryan/src/js/main.js
@@ -7,10 +7,10 @@ function initStickyPhone() {
   const startSection = document.querySelector(".phone-section");
   const phonesWrapper = startSection.querySelector('.sticky-phones-wrapper');
   const phoneContent = phonesWrapper.querySelector('.sticky-phones-content');
-  const kidsWrapper = startSection.querySelector('.sticky-kids-wrapper');
-  const kidsContent = startSection.querySelector('.sticky-kids-content');
+  // const kidsWrapper = startSection.querySelector('.sticky-kids-wrapper');
+  // const kidsContent = startSection.querySelector('.sticky-kids-content');
   const phoneImgs = phoneContent.querySelectorAll('img');
-  const kidsElements = kidsContent.querySelectorAll('[id*=kids_anim]');
+  // const kidsElements = kidsContent.querySelectorAll('[id*=kids_anim]');
   const steps = document.querySelectorAll('.steps li');
   const stepsReact = [...steps].reduce((prev, cur, ind) => {
     const stepRect = cur.getBoundingClientRect();
@@ -19,12 +19,11 @@ function initStickyPhone() {
   }, {})
   const sectionRect = startSection.getBoundingClientRect();
   phoneImgs.forEach((img, ind) => img.style.zIndex = `${ind}`);
-  kidsElements.forEach((el, ind) => el.style.zIndex = `${ind}`);
+  // kidsElements.forEach((el, ind) => el.style.zIndex = `${ind}`);
 
 
   const phoneWrapDesctination = sectionRect.height - stepsReact[0].height + 280;
   const phoneContentHeight = phoneContent.getBoundingClientRect().height;
-  const kidsContentHeight =  kidsContent.getBoundingClientRect().height;
 
   // Phone wrapper scroll anim
   gsap.fromTo(
@@ -39,37 +38,40 @@ function initStickyPhone() {
       rotation: -3,
       skewX: 2,
       scrollTrigger: {
-        trigger: phonesWrapper,
-        start: `${stepsReact[0].height}-=250px center`,
+        trigger: steps[0].querySelector('.title'),
+        start: `50% center`,
         end: `${phoneWrapDesctination} center`,
         scrub: 0.5,
         // markers: true,
         pin: phonesWrapper,
+        onEnter: () => {
+          // kidsAnimation[0].play();
+        }
       },
     }
   );
 
   //Kids wrapper scroll anim
-  gsap.fromTo(
-    kidsWrapper,
-    {
-      x: 0,
-    },
-    {
-      x: 0,
-      scrollTrigger: {
-        trigger: phonesWrapper,
-        start: `${stepsReact[0].height}-=250px center`,
-        end: `${phoneWrapDesctination} center`,
-        scrub: 0.5,
-        // markers: true,
-        pin: kidsWrapper,
-        onEnter: () => {
-          kidsAnimation[0].play()
-        }
-      },
-    }
-  );
+  // gsap.fromTo(
+  //   kidsWrapper,
+  //   {
+  //     x: 0,
+  //   },
+  //   {
+  //     x: 0,
+  //     scrollTrigger: {
+  //       trigger: phonesWrapper,
+  //       start: `${stepsReact[0].height}-=250px center`,
+  //       end: `${phoneWrapDesctination} center`,
+  //       scrub: 0.5,
+  //       // markers: true,
+  //       pin: kidsWrapper,
+  //       onEnter: () => {
+  //         kidsAnimation[0].play()
+  //       }
+  //     },
+  //   }
+  // );
 
   // Content images scroll anim
   phoneImgs.forEach((img, ind) => {
@@ -87,14 +89,28 @@ function initStickyPhone() {
           rotation: 0,
           scrollTrigger: {
             trigger: steps[ind],
-            start: `75% 80%-${phoneContentHeight}`,
-            end: `bottom+=10% 85%`,
+            start: `65% 80%-${phoneContentHeight}`,
+            end: `bottom+=20% 85%`,
             scrub: 1,
             // markers: true,
             onUpdate: (self) => {
               const filterValue = self.progress.toFixed(3) * 20;
               phoneImgs[ind - 1].style.filter = `blur(${filterValue}px)`;
+              if(self.progress < 0.1) {
+                kidsAnimation[ind].pause();
+              } else {
+                if(kidsAnimation[ind].isPaused) {
+                  kidsAnimation[ind].play();
+                }
+              }
             },
+            onLeave: (self) => {
+              kidsAnimation[ind].play();
+              kidsAnimation[ind-1].pause();
+            },
+            onEnterBack: () => {
+              kidsAnimation[ind-1].play();
+            }
           },
         }
       )
@@ -102,41 +118,41 @@ function initStickyPhone() {
   });
 
    // Kids anim scroll sticky
-   kidsElements.forEach((el, ind) => {
-    if (ind > 0) {
-      gsap.fromTo(
-        el,
-        {
-          x: 0,
-          y: -500,
-          rotation: 0,
-        },
-        {
-          x: 0,
-          y: 0,
-          rotation: 0,
-          scrollTrigger: {
-            trigger: steps[ind],
-            start: `75% 80%-${500}`,
-            end: `bottom+=10% 85%`,
-            scrub: 1,
-            // markers: true,
-            onUpdate: (self) => {
-              const filterValue = self.progress.toFixed(3) * 20;
-              kidsElements[ind - 1].style.filter = `blur(${filterValue}px)`;
-              kidsElements[ind - 1].style.opacity = `${1 - self.progress}`;
-              if(self.progress !== 1) {
-                kidsAnimation[ind].pause();
-              }
-            },
-            onLeave: (self) => {
-              kidsAnimation[ind].play();
-            },
-          },
-        }
-      )
-    }
-  });
+  //  kidsElements.forEach((el, ind) => {
+  //   if (ind > 0) {
+  //     gsap.fromTo(
+  //       el,
+  //       {
+  //         x: 0,
+  //         y: -500,
+  //         rotation: 0,
+  //       },
+  //       {
+  //         x: 0,
+  //         y: 0,
+  //         rotation: 0,
+  //         scrollTrigger: {
+  //           trigger: steps[ind],
+  //           start: `75% 80%-${500}`,
+  //           end: `bottom+=10% 85%`,
+  //           scrub: 1,
+  //           // markers: true,
+  //           onUpdate: (self) => {
+  //             const filterValue = self.progress.toFixed(3) * 20;
+  //             kidsElements[ind - 1].style.filter = `blur(${filterValue}px)`;
+  //             kidsElements[ind - 1].style.opacity = `${1 - self.progress}`;
+  //             if(self.progress !== 1) {
+  //               kidsAnimation[ind].pause();
+  //             }
+  //           },
+  //           onLeave: (self) => {
+  //             kidsAnimation[ind].play();
+  //           },
+  //         },
+  //       }
+  //     )
+  //   }
+  // });
 }
 
 /* ------------------- card section carousel ----------------*/
@@ -238,3 +254,21 @@ const kidsAnimation = new Array(5).fill('kids_anim').map((elem, ind, arr) => {
   });
   return anim;
 });
+
+function callback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      kidsAnimation[0].play();
+    }
+  });
+}
+function createObserver(target, callback) {
+  const options = {
+    root: null,
+    threshold: 0,
+  };
+  const observer = new IntersectionObserver(callback, options);
+  observer.observe(target);
+}
+
+createObserver(document.querySelector('#kids_anim_1'), callback);
