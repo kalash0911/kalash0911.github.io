c53d46917b2bcfab2ae351cf58e575053c41c78b
Anton Levchenko <ICAnton.Levchenko@americanwell.com>
2023-08-21 20:40:34 +0300

    changed animation

--- Ryan/dist/js/main.js
+++ Ryan/dist/js/main.js
@@ -13,7 +13,6 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToAr
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 window.addEventListener("load", function (event) {
-  circleImageAnimation();
   initStickyPhone();
 });
 
@@ -157,96 +156,6 @@ function initStickyPhone() {
   //     )
   //   }
   // });
-}
-/* ------------------- card section carousel ----------------*/
-
-/* ------------------- triangle degrees helpers ----------------*/
-
-
-var radToDegree = function radToDegree(radians) {
-  return radians * (180 / Math.PI);
-};
-
-var calcIsoscelesTriangleDegree = function calcIsoscelesTriangleDegree(a, b) {
-  var cosA = b / (2 * a);
-  var aDeg = radToDegree(Math.acos(cosA));
-  var bDeg = 180 - aDeg * 2;
-  return {
-    cosA: cosA,
-    aDeg: aDeg,
-    bDeg: bDeg
-  };
-};
-
-var calcRightAngledTriangleDegree = function calcRightAngledTriangleDegree(a, b) {
-  var tanA = a / b;
-  var aDeg = radToDegree(Math.atan(tanA));
-  var bDeg = 180 - 90 - aDeg;
-  return {
-    tanA: tanA,
-    aDeg: aDeg,
-    bDeg: bDeg
-  };
-};
-
-var calcInitRotation = function calcInitRotation(rightAngledTriangleBDeg, isoscelesTriangleBDeg) {
-  // hardcoded fix for mobile
-  var correction = 0;
-
-  if (window.innerWidth <= 1024) {
-    correction = 6;
-  }
-
-  if (window.innerWidth <= 480) {
-    correction = 3;
-  }
-
-  return rightAngledTriangleBDeg / 2 + isoscelesTriangleBDeg - correction;
-};
-
-var calcFinalRotation = function calcFinalRotation(rightAngledTriangleBDeg, isoscelesTriangleBDeg, cellsLength) {
-  var correction = 0;
-
-  if (window.innerWidth <= 1024) {
-    correction = -3;
-  }
-
-  if (window.innerWidth <= 480) {
-    correction = -1;
-  }
-
-  return -(isoscelesTriangleBDeg * cellsLength - rightAngledTriangleBDeg * 2) - rightAngledTriangleBDeg / 2 - correction;
-};
-/* ------------------- card section initialization ----------------*/
-
-
-function circleImageAnimation() {
-  var wrapper = document.querySelector(".card-section");
-  var cardHolder = document.querySelector(".card-holder");
-  var cells = document.querySelectorAll(".card-holder .item");
-  if (!wrapper || !cardHolder || !cells) return;
-
-  var _calcIsoscelesTriangl = calcIsoscelesTriangleDegree(cardHolder.offsetWidth / 2 - cells[0].offsetHeight, cells[0].offsetWidth),
-      IsoscelesTriangleBDeg = _calcIsoscelesTriangl.bDeg;
-
-  var _calcRightAngledTrian = calcRightAngledTriangleDegree(wrapper.offsetWidth / 2, cardHolder.offsetWidth / 2),
-      RightAngledTriangleBDeg = _calcRightAngledTrian.aDeg;
-
-  gsap.fromTo(cardHolder, {
-    x: 0,
-    rotation: -calcInitRotation(RightAngledTriangleBDeg, IsoscelesTriangleBDeg)
-  }, {
-    x: 0,
-    rotation: calcFinalRotation(RightAngledTriangleBDeg, IsoscelesTriangleBDeg, cells.length) > 0 ? 0 : calcFinalRotation(RightAngledTriangleBDeg, IsoscelesTriangleBDeg, cells.length),
-    scrollTrigger: {
-      trigger: wrapper,
-      start: '30% 50%',
-      pin: ".carousel-wrap",
-      end: "bottom",
-      scrub: 0.5 // markers: true,
-
-    }
-  });
 }
 
 var kidsAnimation = new Array(5).fill('kids_anim').map(function (elem, ind, arr) {
