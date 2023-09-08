0a1b9a02e851a896a957c0c403c100e3fc728693
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-09-08 14:42:31 +0300

    added mov

--- Ryan/dist/js/main.js
+++ Ryan/dist/js/main.js
@@ -1,6 +1,27 @@
 "use strict";
 
-// Video controls
+function supportsHEVCAlpha() {
+  var navigator = window.navigator;
+  var ua = navigator.userAgent.toLowerCase();
+  var hasMediaCapabilities = !!(navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo);
+  var isSafari = ua.indexOf('safari') != -1 && !(ua.indexOf('chrome') != -1) && ua.indexOf('version/') != -1;
+  var isiphone = /iPad|iPhone|iPod/.test(navigator.userAgent) || navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1;
+  return (isSafari || isiphone) && hasMediaCapabilities;
+}
+
+var playerKids = document.querySelectorAll('.kids');
+var playerStep = document.querySelectorAll('.img-step');
+var playerStepMob = document.querySelectorAll('.img-step-mob');
+playerKids.forEach(function (video, ind) {
+  video.src = supportsHEVCAlpha() ? "./files/".concat(ind + 1, ".mov") : "./files/".concat(ind + 1, ".webm");
+});
+playerStep.forEach(function (video, ind) {
+  video.src = supportsHEVCAlpha() ? "./files/step".concat(ind + 1, ".mp4") : "./files/step".concat(ind + 1, ".webm");
+});
+playerStepMob.forEach(function (video, ind) {
+  video.src = supportsHEVCAlpha() ? "./files/step".concat(ind + 1, "-mob.mp4") : "./files/step".concat(ind + 1, "-mob.webm");
+}); // Video controls
+
 initVideoPlayers();
 
 function initVideoPlayers() {
