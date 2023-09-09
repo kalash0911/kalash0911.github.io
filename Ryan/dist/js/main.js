5434e0891188e1604ef612eb1de67bc8ababdfe9
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-09-09 16:27:44 +0300

    added new sec

--- Ryan/dist/js/main.js
+++ Ryan/dist/js/main.js
@@ -40,5 +40,122 @@ function initVideoPlayers() {
       }
     });
   });
+} // Video controls pop-up
+
+
+initVideoPlayersPopUp();
+
+function initVideoPlayersPopUp() {
+  var videoWraps = document.querySelectorAll(".video-wrap-pop-up");
+  videoWraps === null || videoWraps === void 0 ? void 0 : videoWraps.forEach(function (videoWrap) {
+    var video = videoWrap.querySelector(".video");
+    var playButton = videoWrap.querySelector(".play-button");
+    var muteButton = videoWrap.querySelector(".mute-button");
+    var volume = videoWrap.querySelector(".volume");
+    var currentTimeElement = videoWrap.querySelector(".current");
+    var durationTimeElement = videoWrap.querySelector(".duration");
+    var progress = videoWrap.querySelector(".video-progress");
+    var progressBar = videoWrap.querySelector(".video-progress-filled"); //Play and Pause button
+
+    playButton.addEventListener("click", function (e) {
+      if (video.paused) {
+        video.play();
+        videoWrap.classList.toggle("active");
+      } else {
+        video.pause();
+        videoWrap.classList.toggle("active");
+      }
+    }); //Mute button
+
+    muteButton.addEventListener("click", function (e) {
+      if (video.muted) {
+        video.muted = false;
+        muteButton.classList.toggle("mute");
+      } else {
+        video.muted = true;
+        muteButton.classList.toggle("mute");
+      }
+    }); //volume
+
+    volume.addEventListener("mousemove", function (e) {
+      video.volume = e.target.value;
+    }); //current time and duration
+
+    var currentTime = function currentTime() {
+      var currentMinutes = Math.floor(video.currentTime / 60);
+      var currentSeconds = Math.floor(video.currentTime - currentMinutes * 60);
+      var durationMinutes = Math.floor(video.duration / 60);
+      var durationSeconds = Math.floor(video.duration - durationMinutes * 60);
+      currentTimeElement.innerHTML = "".concat(currentMinutes, ":").concat(currentSeconds < 10 ? "0" + currentSeconds : currentSeconds);
+      durationTimeElement.innerHTML = "".concat(durationMinutes, ":").concat(durationSeconds);
+    };
+
+    video.addEventListener("timeupdate", currentTime); //Progress bar
+
+    video.addEventListener("timeupdate", function () {
+      var percentage = video.currentTime / video.duration * 100;
+      progressBar.style.width = "".concat(percentage, "%");
+    }); //change progress bar on click
+
+    progress.addEventListener("click", function (e) {
+      var progressTime = e.offsetX / progress.offsetWidth * video.duration;
+      video.currentTime = progressTime;
+    });
+    video.addEventListener("ended", stopMedia);
+
+    function stopMedia() {
+      video.pause();
+      video.currentTime = 0;
+      videoWrap.classList.remove("active");
+    }
+  });
 }
+/* Modal popUp img */
+
+
+var itemImg = document.querySelectorAll('.gallery__item-img');
+var modal = document.querySelector('.modal-pop-up');
+var modalImg = modal.querySelector('img');
+var overflowImg = document.querySelector('.modal-overflow');
+var closeBtnImg = document.querySelector('.modal-btn-close');
+
+function showImg(e) {
+  document.body.classList.add("body_lock");
+  modal.classList.add("modal-img_active");
+  e.preventDefault();
+  var img = e.target.closest('img');
+  var src = img.getAttribute('src');
+  modalImg.setAttribute('src', src);
+}
+
+;
+
+function closeImg() {
+  document.body.classList.remove("body_lock");
+  modal.classList.remove("modal-img_active");
+}
+
+;
+
+for (var i = 0; i < itemImg.length; ++i) {
+  itemImg[i].addEventListener("click", function (e) {
+    showImg(e);
+  });
+}
+
+;
+var modalActive = document.querySelectorAll('.modal-img_active');
+
+if (modalActive) {
+  overflowImg.addEventListener("click", function (e) {
+    closeImg();
+    e.preventDefault();
+  });
+  closeBtnImg.addEventListener("click", function (e) {
+    closeImg();
+    e.preventDefault();
+  });
+}
+
+;
 //# sourceMappingURL=main.js.map
--- Ryan/src/js/main.js
+++ Ryan/src/js/main.js
@@ -41,3 +41,125 @@ function initVideoPlayers() {
 
     });
 }
+
+// Video controls pop-up
+initVideoPlayersPopUp();
+
+function initVideoPlayersPopUp() {
+    const videoWraps = document.querySelectorAll(".video-wrap-pop-up");
+    videoWraps?.forEach((videoWrap) => {
+        const video = videoWrap.querySelector(".video");
+        const playButton = videoWrap.querySelector(".play-button");
+        const muteButton = videoWrap.querySelector(".mute-button");
+        const volume = videoWrap.querySelector(".volume");
+        const currentTimeElement = videoWrap.querySelector(".current");
+        const durationTimeElement = videoWrap.querySelector(".duration");
+        const progress = videoWrap.querySelector(".video-progress");
+        const progressBar = videoWrap.querySelector(".video-progress-filled");
+
+        //Play and Pause button
+        playButton.addEventListener("click", (e) => {
+            if (video.paused) {
+                video.play();
+                videoWrap.classList.toggle("active");
+            } else {
+                video.pause();
+                videoWrap.classList.toggle("active");
+            }
+        });
+
+        //Mute button
+        muteButton.addEventListener("click", (e) => {
+            if (video.muted) {
+                video.muted = false;
+                muteButton.classList.toggle("mute");
+            } else {
+                video.muted = true;
+                muteButton.classList.toggle("mute");
+            }
+        });
+
+        //volume
+        volume.addEventListener("mousemove", (e) => {
+            video.volume = e.target.value;
+        });
+
+        //current time and duration
+        const currentTime = () => {
+            let currentMinutes = Math.floor(video.currentTime / 60);
+            let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60);
+            let durationMinutes = Math.floor(video.duration / 60);
+            let durationSeconds = Math.floor(video.duration - durationMinutes * 60);
+
+            currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? "0" + currentSeconds : currentSeconds
+                }`;
+            durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`;
+        };
+
+        video.addEventListener("timeupdate", currentTime);
+
+        //Progress bar
+        video.addEventListener("timeupdate", () => {
+            const percentage = (video.currentTime / video.duration) * 100;
+            progressBar.style.width = `${percentage}%`;
+        });
+
+        //change progress bar on click
+        progress.addEventListener("click", (e) => {
+            const progressTime = (e.offsetX / progress.offsetWidth) * video.duration;
+            video.currentTime = progressTime;
+        });
+
+        video.addEventListener("ended", stopMedia);
+        function stopMedia() {
+            video.pause();
+            video.currentTime = 0;
+            videoWrap.classList.remove("active");
+        }
+
+    });
+}
+
+/* Modal popUp img */
+
+const itemImg = document.querySelectorAll('.gallery__item-img');
+const modal = document.querySelector('.modal-pop-up');
+const modalImg = modal.querySelector('img');
+const overflowImg = document.querySelector('.modal-overflow');
+const closeBtnImg = document.querySelector('.modal-btn-close');
+
+function showImg(e) {
+    document.body.classList.add("body_lock");
+    modal.classList.add("modal-img_active");
+    e.preventDefault();
+
+    let img = e.target.closest('img');
+    let src = img.getAttribute('src');
+
+    modalImg.setAttribute('src', src);
+};
+
+function closeImg() {
+    document.body.classList.remove("body_lock");
+    modal.classList.remove("modal-img_active");
+};
+
+for (let i = 0; i < itemImg.length; ++i) {
+    itemImg[i].addEventListener("click", function (e) {
+        showImg(e);
+    });
+};
+
+let modalActive = document.querySelectorAll('.modal-img_active');
+
+if (modalActive) {
+    overflowImg.addEventListener("click", function (e) {
+        closeImg();
+        e.preventDefault();
+    });
+
+    closeBtnImg.addEventListener("click", function (e) {
+        closeImg();
+        e.preventDefault();
+    });
+};
