79a9b02edc4197511f51d7fe8c29a347d0e7a898
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-09-16 16:57:47 +0300

    added new sections for sales course project

--- SalesCourse/gulp/tasks/files.js
+++ SalesCourse/gulp/tasks/files.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 
 module.exports = function files() {
   gulp.src('src/*.txt')
-  .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'));
   return gulp.src('src/files/*')
     .pipe(gulp.dest('dist/files'));
 };
