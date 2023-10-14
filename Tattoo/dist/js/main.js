e11ffe7333412ff822127438d35cb08929d03b03
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-10-15 01:41:58 +0300

    added new sc for tattoo project

--- Tattoo/dist/js/main.js
+++ Tattoo/dist/js/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -11,13 +17,15 @@ new WOW().init(); // Swiper:
 function destroySlidersOnResize(selector, width, obj, moreThan) {
   var init = _objectSpread({}, obj);
 
+  var win = window;
+  var sliderSelector = document.querySelector(selector);
+  var swiper = new Swiper(selector, init);
+
   var toggleInit = function toggleInit() {
     var neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width;
 
     if (neededWidth) {
-      var _sliderSelector;
-
-      if (!((_sliderSelector = sliderSelector) === null || _sliderSelector === void 0 ? void 0 : _sliderSelector.classList.contains("swiper-initialized"))) {
+      if (!(sliderSelector === null || sliderSelector === void 0 ? void 0 : sliderSelector.classList.contains("swiper-initialized"))) {
         swiper = new Swiper(selector, init);
       }
     } else if (sliderSelector.classList.contains("swiper-initialized")) {
@@ -28,12 +36,46 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
   ["load", "resize"].forEach(function (evt) {
     return win.addEventListener(evt, toggleInit, false);
   });
+  return swiper;
 }
 
-destroySlidersOnResize(".me-slider", 960, {
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination"
+var howSlider = destroySlidersOnResize(".slider-how", 99999, {
+  speed: 0,
+  thumbs: {
+    swiper: {
+      el: '.slider-how-nav',
+      slidesPerView: 4
+    }
   }
 });
+destroySlidersOnResize(".slider-how-nav", 1, {});
+/* castom anim */
+
+function onEntry(entry) {
+  entry.forEach(function (change) {
+    if (change.isIntersecting) {
+      change.target.classList.add('show');
+    }
+  });
+}
+
+var options = {
+  threshold: [0.5]
+};
+var observer = new IntersectionObserver(onEntry, options);
+var elements = document.querySelectorAll('.anim');
+
+var _iterator = _createForOfIteratorHelper(elements),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var elm = _step.value;
+    observer.observe(elm);
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
 //# sourceMappingURL=main.js.map
--- Tattoo/src/js/main.js
+++ Tattoo/src/js/main.js
@@ -7,6 +7,10 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
     ...obj,
   };
 
+  const win = window;
+  const sliderSelector = document.querySelector(selector);
+  let swiper = new Swiper(selector, init);
+
   const toggleInit = () => {
     const neededWidth = moreThan
       ? win.innerWidth >= width
@@ -23,12 +27,36 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
   ["load", "resize"].forEach((evt) =>
     win.addEventListener(evt, toggleInit, false)
   );
+
+  return swiper;
 }
 
-destroySlidersOnResize(".me-slider", 960, {
-  spaceBetween: 20,
+const howSlider = destroySlidersOnResize(".slider-how", 99999, {
+  speed: 0,
 
-  pagination: {
-    el: ".swiper-pagination",
+  thumbs: {
+    swiper: {
+      el: '.slider-how-nav',
+      slidesPerView: 4,
+    },
   },
+
 });
+
+destroySlidersOnResize(".slider-how-nav", 1, {});
+
+/* castom anim */
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.anim');
+for (let elm of elements) {
+  observer.observe(elm);
+}
