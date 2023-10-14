3e24af6a7f3a54d1794e65086cf61d9f75d5dd57
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-10-14 19:31:04 +0300

    added new project tattoo

---
+++ Tattoo/src/js/main.js
@@ -0,0 +1,34 @@
+new WOW().init();
+
+// Swiper:
+
+function destroySlidersOnResize(selector, width, obj, moreThan) {
+  const init = {
+    ...obj,
+  };
+
+  const toggleInit = () => {
+    const neededWidth = moreThan
+      ? win.innerWidth >= width
+      : win.innerWidth <= width;
+    if (neededWidth) {
+      if (!sliderSelector?.classList.contains("swiper-initialized")) {
+        swiper = new Swiper(selector, init);
+      }
+    } else if (sliderSelector.classList.contains("swiper-initialized")) {
+      swiper.destroy();
+    }
+  };
+
+  ["load", "resize"].forEach((evt) =>
+    win.addEventListener(evt, toggleInit, false)
+  );
+}
+
+destroySlidersOnResize(".me-slider", 960, {
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
