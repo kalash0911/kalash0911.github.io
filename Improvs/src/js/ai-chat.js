7c58eb0498511e972ef7d1b07a0c2b8e80952bb1
mishachto <solonyna.misha@gmail.com>
2024-03-15 10:33:05 +0200

    Add modal chat for all pages.

---
+++ Improvs/src/js/ai-chat.js
@@ -0,0 +1,15 @@
+function toggleActiveClass() {
+    var openBtn = document.querySelector('.open-btn-ai-chat');
+    if (openBtn) {
+        openBtn.addEventListener('click', function () {
+            var aiChatBox = this.closest('.ai-chat-box');
+            if (aiChatBox) {
+                aiChatBox.classList.toggle('active');
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    toggleActiveClass();
+});
