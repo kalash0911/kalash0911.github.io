6f944854d21561038d9181c79397f1c5c629d077
mishachto <solonyna.misha@gmail.com>
2024-06-16 18:32:23 +0300

    Add modal chat for site

--- Improvs/dist/js/ai-chat.js
+++ Improvs/dist/js/ai-chat.js
@@ -1,15 +1,33 @@
 "use strict";
 
 function toggleActiveClass() {
-  var openBtn = document.querySelector('.open-btn-ai-chat');
-  if (openBtn) {
-    openBtn.addEventListener('click', function () {
-      var aiChatBox = this.closest('.ai-chat-box');
-      if (aiChatBox) {
-        aiChatBox.classList.toggle('active');
-      }
+  var buttons = document.querySelectorAll('.open-btn-ai-chat');
+  var close = document.querySelector('.close-btn-chat');
+  var chatBoxes = document.querySelectorAll('.ai-chat-box');
+  close.addEventListener('click', function () {
+    chatBoxes.forEach(function (chatBox) {
+      chatBox.classList.toggle('active');
+    });
+  });
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var iframe = document.getElementById('iframe-chat');
+      var modile = window.innerWidth < 768;
+      iframe.src = "http://localhost:3002/chat-public/modal/asst_1wlULcYq0yd3bbMzAAulmZEL/".concat(modile);
+      setTimeout(function () {
+        chatBoxes.forEach(function (chatBox) {
+          chatBox.classList.toggle('active');
+        });
+      }, 100);
+    });
+  });
+  function removeClassOnResize() {
+    chatBoxes.forEach(function (element) {
+      element.classList.remove('active');
     });
   }
+  window.addEventListener('resize', removeClassOnResize);
+  removeClassOnResize();
 }
 document.addEventListener('DOMContentLoaded', function () {
   toggleActiveClass();
--- Improvs/src/js/ai-chat.js
+++ Improvs/src/js/ai-chat.js
@@ -1,14 +1,40 @@
 function toggleActiveClass() {
-    var openBtn = document.querySelector('.open-btn-ai-chat');
-    if (openBtn) {
-        openBtn.addEventListener('click', function () {
-            var aiChatBox = this.closest('.ai-chat-box');
-            if (aiChatBox) {
-                aiChatBox.classList.toggle('active');
-            }
+    const buttons = document.querySelectorAll('.open-btn-ai-chat');
+    const close = document.querySelector('.close-btn-chat');
+    const chatBoxes = document.querySelectorAll('.ai-chat-box');
+
+    close.addEventListener('click', function () {
+        chatBoxes.forEach(chatBox => {
+            chatBox.classList.toggle('active');
+        });
+    });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+
+            const iframe = document.getElementById('iframe-chat');
+            const modile = window.innerWidth < 768
+            iframe.src = `http://localhost:3002/chat-public/modal/asst_1wlULcYq0yd3bbMzAAulmZEL/${modile}`;
+            setTimeout(() => {
+                chatBoxes.forEach(chatBox => {
+                    chatBox.classList.toggle('active');
+                });
+            }, 100);
+        });
+    });
+
+
+    function removeClassOnResize() {
+
+        chatBoxes.forEach(function (element) {
+            element.classList.remove('active');
         });
     }
+
+    window.addEventListener('resize', removeClassOnResize);
+    removeClassOnResize();
 }
+
 
 document.addEventListener('DOMContentLoaded', function () {
     toggleActiveClass();
